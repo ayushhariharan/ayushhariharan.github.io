2c3b488a4eca026bda4fa0c4b353be08c69db273
ahariharan <ahariharan@berkeley.edu>
2022-08-03 16:30:51 -0400

    finished website (up to summer 2022)

--- assets/js/roadmap.js
+++ assets/js/roadmap.js
@@ -22,6 +22,26 @@
 */
 //=====================================//
 
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
+
 $('div#dropdown').on('click', function(){
 	var $map_type=$(this).attr('data-filter')
 
